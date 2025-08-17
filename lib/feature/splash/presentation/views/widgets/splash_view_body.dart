2f8691691424695eed2d7a76e3fdef8026c47b33
mohamed reda <rdam6837@gmail.com>
2025-08-18 00:24:18 +0300

    first commit

---
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:book_app/core/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(Assets.logo),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:book_app/core/style.dart';
+import 'package:book_app/feature/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: mainColor),
+      home: const SplashView(),
+    );
+  }
+}
