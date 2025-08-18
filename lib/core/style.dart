5bf3fdedf9c61e2b645695cbd7e8632b4a07128b
mohamed reda <rdam6837@gmail.com>
2025-08-18 23:32:45 +0300

    adding animation & namvigate to home view

--- lib/core/style.dart
+++ lib/core/style.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const mainColor = Color(0xff100B20);
+const kTranstionDuration = Duration(microseconds: 100);
--- lib/feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,50 @@
 import 'package:book_app/core/assets.dart';
+import 'package:book_app/core/style.dart';
+import 'package:book_app/feature/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateHome();
+  }
+
+  void navigateHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(const HomeView(),
+          transition: Transition.fade, duration: kTranstionDuration);
+    });
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 12), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  // ignore: unnecessary_overrides
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +54,17 @@ class SplashViewBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Image.asset(Assets.logo),
-          const Text(
-            'Reed Free Books',
-            textAlign: TextAlign.center,
-          ),
+          AnimatedBuilder(
+              animation: slidingAnimation,
+              builder: (context, _) {
+                return SlideTransition(
+                  position: slidingAnimation,
+                  child: const Text(
+                    'Reed Free Books',
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              }),
         ],
       ),
     );
